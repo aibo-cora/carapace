5a3ef1b8ee151c27bff336c80ca01e89c324bfca
Yura Filatov <yura.fila@gmail.com>
2025-07-13 17:38:37 +0300

    Initial Commit

---
+++ CarapaceTests/CarapaceTests.swift
@@ -0,0 +1,16 @@
+//
+//  CarapaceTests.swift
+//  CarapaceTests
+//
+//  Created by Yura on 7/13/25.
+//
+
+import Testing
+
+struct CarapaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
